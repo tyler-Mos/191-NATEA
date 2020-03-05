97a7e899c7783ac81ffc39f7047362f00ad70252
yingshanguan <yingshag@uci.edu>
2020-03-04 16:28:39 -0800

    try to fix the error page

---
+++ natea-webapp-project/src/main/java/com/example/natea/nateawebappproject/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.natea.nateawebappproject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class HelloController{
+
+    @RequestMapping("/")
+    public String index(){
+        return "Greetings from Spring Boot!";
+    }
+}
