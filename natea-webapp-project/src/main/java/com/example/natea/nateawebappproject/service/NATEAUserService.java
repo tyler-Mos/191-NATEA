2db98fa11f15106eee4b3606f207036ae82a0acc
Abigail Yang <yangyingyu10@gmail.com>
2020-05-08 04:24:00 -0700

    fixed bug in user service

--- natea-webapp-project/src/main/java/com/example/natea/nateawebappproject/service/NATEAUserService.java
+++ natea-webapp-project/src/main/java/com/example/natea/nateawebappproject/service/NATEAUserService.java
@@ -6,7 +6,9 @@ import com.example.natea.nateawebappproject.model.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class NATEAUserService implements IUserService {
 
     @Autowired
